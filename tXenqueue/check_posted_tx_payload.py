7a48ba955691176e353b598b641682eed676595b
Robert Hunt <Freely.Given.org@gmail.com>
2018-09-24 11:48:18 +1200

    Remove unneed callback

--- tXenqueue/check_posted_tx_payload.py
+++ tXenqueue/check_posted_tx_payload.py
@@ -74,62 +74,62 @@ def check_posted_tx_payload(request):
 
 
 
-def check_posted_callback_payload(request):
-    """
-    Accepts callback notification from TX.
-        Parameter is a rq request object
+#def check_posted_callback_payload(request):
+    #"""
+    #Accepts callback notification from TX.
+        #Parameter is a rq request object
 
-    Returns a 2-tuple:
-        True or False if payload checks out
-        The payload that was checked or error dict
-    """
-    # Bail if this is not a POST with a payload
-    if not request.data:
-        logging.error("Received request but no payload found")
-        return False, {'error': 'No payload found. You must submit a POST request'}
+    #Returns a 2-tuple:
+        #True or False if payload checks out
+        #The payload that was checked or error dict
+    #"""
+    ## Bail if this is not a POST with a payload
+    #if not request.data:
+        #logging.error("Received request but no payload found")
+        #return False, {'error': 'No payload found. You must submit a POST request'}
 
-    # TODO: What headers do we need to check ???
-    ## Bail if this is not from tX
-    #if 'X-Gogs-Event' not in request.headers:
-        #logging.error(f"Cannot find 'X-Gogs-Event' in {request.headers}")
-        #return False, {'error': 'This does not appear to be from tX.'}
+    ## TODO: What headers do we need to check ???
+    ### Bail if this is not from tX
+    ##if 'X-Gogs-Event' not in request.headers:
+        ##logging.error(f"Cannot find 'X-Gogs-Event' in {request.headers}")
+        ##return False, {'error': 'This does not appear to be from tX.'}
 
-    ## Bail if this is not a push event
-    #if not request.headers['X-Gogs-Event'] == 'push':
-        #logging.error(f"X-Gogs-Event is not a push in {request.headers}")
-        #return False, {'error': 'This does not appear to be a push.'}
+    ### Bail if this is not a push event
+    ##if not request.headers['X-Gogs-Event'] == 'push':
+        ##logging.error(f"X-Gogs-Event is not a push in {request.headers}")
+        ##return False, {'error': 'This does not appear to be a push.'}
 
-    # Get the json payload and check it
-    payload_json = request.get_json()
-    print( "callback payload is", repr(payload_json))
+    ## Get the json payload and check it
+    #payload_json = request.get_json()
+    #print( "callback payload is", repr(payload_json))
 
-    # TODO: What payload info do we need to check and to match to a job
-    ## Bail if the URL to the repo is invalid
-    #try:
-        #if not payload_json['repository']['html_url'].startswith(GOGS_URL):
-            #logging.error(f"The repo at {payload_json['repository']['html_url']!r} does not belong to {GOGS_URL!r}")
-            #return False, {'error': f'The repo does not belong to {GOGS_URL}.'}
-    #except KeyError:
-        #logging.error("No repo URL specified")
-        #return False, {'error': 'No repo URL specified.'}
+    ## TODO: What payload info do we need to check and to match to a job
+    ### Bail if the URL to the repo is invalid
+    ##try:
+        ##if not payload_json['repository']['html_url'].startswith(GOGS_URL):
+            ##logging.error(f"The repo at {payload_json['repository']['html_url']!r} does not belong to {GOGS_URL!r}")
+            ##return False, {'error': f'The repo does not belong to {GOGS_URL}.'}
+    ##except KeyError:
+        ##logging.error("No repo URL specified")
+        ##return False, {'error': 'No repo URL specified.'}
 
-    ## Bail if the commit branch is not the default branch
-    #try:
-        #commit_branch = payload_json['ref'].split('/')[2]
-    #except (IndexError, AttributeError):
-        #logging.error(f"Could not determine commit branch from {payload_json['ref']}")
-        #return False, {'error': 'Could not determine commit branch.'}
-    #except KeyError:
-        #logging.error("No commit branch specified")
-        #return False, {'error': 'No commit branch specified.'}
-    #try:
-        #if commit_branch != payload_json['repository']['default_branch']:
-            #logging.error(f'Commit branch: {commit_branch} is not the default branch')
-            #return False, {'error': f'Commit branch: {commit_branch} is not the default branch.'}
-    #except KeyError:
-        #logging.error("No default branch specified")
-        #return False, {'error': 'No default branch specified.'}
+    ### Bail if the commit branch is not the default branch
+    ##try:
+        ##commit_branch = payload_json['ref'].split('/')[2]
+    ##except (IndexError, AttributeError):
+        ##logging.error(f"Could not determine commit branch from {payload_json['ref']}")
+        ##return False, {'error': 'Could not determine commit branch.'}
+    ##except KeyError:
+        ##logging.error("No commit branch specified")
+        ##return False, {'error': 'No commit branch specified.'}
+    ##try:
+        ##if commit_branch != payload_json['repository']['default_branch']:
+            ##logging.error(f'Commit branch: {commit_branch} is not the default branch')
+            ##return False, {'error': f'Commit branch: {commit_branch} is not the default branch.'}
+    ##except KeyError:
+        ##logging.error("No default branch specified")
+        ##return False, {'error': 'No default branch specified.'}
 
-    logging.info("Callback payload seems ok")
-    return True, payload_json
-# end of check_posted_callback_payload
+    #logging.info("Callback payload seems ok")
+    #return True, payload_json
+## end of check_posted_callback_payload
--- tXenqueue/tx_enqueue_main.py
+++ tXenqueue/tx_enqueue_main.py
@@ -21,13 +21,12 @@ from statsd import StatsClient # Graphite front-end
 from check_posted_tx_payload import check_posted_tx_payload, check_posted_callback_payload
 
 
-OUR_NAME = 'tx' # Becomes the (perhaps prefixed) queue name (and graphite name) -- MUST match setup.py in tx-job-handler
-CALLBACK_SUFFIX = '_callback'
+OUR_NAME = 'tX_webhook' # Becomes the (perhaps prefixed) queue name (and graphite name) -- MUST match setup.py in tx-job-handler
+#CALLBACK_SUFFIX = '_callback'
 
 # NOTE: The following strings if not empty, MUST have a trailing slash but NOT a leading one.
-#WEBHOOK_URL_SEGMENT = 'client/webhook/'
 WEBHOOK_URL_SEGMENT = '' # Leaving this blank will cause the service to run at '/'
-CALLBACK_URL_SEGMENT = WEBHOOK_URL_SEGMENT + 'callback/'
+#CALLBACK_URL_SEGMENT = WEBHOOK_URL_SEGMENT + 'callback/'
 
 JOB_TIMEOUT = '200s' # Then a running job (taken out of the queue) will be considered to have failed
     # NOTE: This is only the time until webhook.py returns after submitting the jobs
@@ -164,61 +163,61 @@ def job_receiver():
 # end of job_receiver()
 
 
-@app.route('/'+CALLBACK_URL_SEGMENT, methods=['POST'])
-def callback_receiver():
-    """
-    Accepts POST requests and checks the (json) payload
+#@app.route('/'+CALLBACK_URL_SEGMENT, methods=['POST'])
+#def callback_receiver():
+    #"""
+    #Accepts POST requests and checks the (json) payload
 
-    Queues the approved jobs at redis instance at global redis_hostname:6379.
-    Queue name is our_callback_name (may have been prefixed).
-    """
-    if request.method == 'POST':
-        stats_client.incr('TotalCallbackPostsReceived')
-        logging.info(f"Enqueue received callback request: {request}")
-        response_ok_flag, response_dict = check_posted_callback_payload(request) # response_dict is json payload if successful, else error info
+    #Queues the approved jobs at redis instance at global redis_hostname:6379.
+    #Queue name is our_callback_name (may have been prefixed).
+    #"""
+    #if request.method == 'POST':
+        #stats_client.incr('TotalCallbackPostsReceived')
+        #logging.info(f"Enqueue received callback request: {request}")
+        #response_ok_flag, response_dict = check_posted_callback_payload(request) # response_dict is json payload if successful, else error info
 
-        if response_ok_flag:
-            # Collect (and log) some helpful information
-            stats_client.incr('GoodCallbackPostsReceived')
-            redis_connection = StrictRedis(host=redis_hostname)
-            our_queue = Queue(our_callback_name, connection=redis_connection)
-            len_our_queue = len(our_queue)
-            stats_client.gauge(prefix+'QueueLength', len_our_queue)
-            failed_queue = Queue('failed', connection=redis_connection)
-            len_failed_queue = len(failed_queue)
-            stats_client.gauge('FailedQueueLength', len_failed_queue)
-            # NOTE: No ttl specified on the next line -- this seems to cause unrun jobs to be just silently dropped
-            #           (For now at least, we prefer them to just stay in the queue if they're not getting processed.)
-            #       The timeout value determines the max run time of the worker once the job is accessed
-            our_queue.enqueue('callback.job', response_dict, timeout=JOB_TIMEOUT) # A function named callback.job will be called by the worker
-            # NOTE: The above line can return a result from the callback.job function. (By default, the result remains available for 500s.)
+        #if response_ok_flag:
+            ## Collect (and log) some helpful information
+            #stats_client.incr('GoodCallbackPostsReceived')
+            #redis_connection = StrictRedis(host=redis_hostname)
+            #our_queue = Queue(our_callback_name, connection=redis_connection)
+            #len_our_queue = len(our_queue)
+            #stats_client.gauge(prefix+'QueueLength', len_our_queue)
+            #failed_queue = Queue('failed', connection=redis_connection)
+            #len_failed_queue = len(failed_queue)
+            #stats_client.gauge('FailedQueueLength', len_failed_queue)
+            ## NOTE: No ttl specified on the next line -- this seems to cause unrun jobs to be just silently dropped
+            ##           (For now at least, we prefer them to just stay in the queue if they're not getting processed.)
+            ##       The timeout value determines the max run time of the worker once the job is accessed
+            #our_queue.enqueue('callback.job', response_dict, timeout=JOB_TIMEOUT) # A function named callback.job will be called by the worker
+            ## NOTE: The above line can return a result from the callback.job function. (By default, the result remains available for 500s.)
 
-            other_callback_queue = Queue(other_our_adjusted_callback_name, connection=redis_connection)
+            #other_callback_queue = Queue(other_our_adjusted_callback_name, connection=redis_connection)
 
-            #workers = Worker.all(connection=redis_connection) # Returns the actual worker objects
-            #logging.debug(f"Total rq workers ({len(workers)}): {workers}")
-            #our_queue_workers = Worker.all(queue=our_queue)
-            #logging.debug(f"Our {our_callback_name} queue workers ({len(our_queue_workers)}): {our_queue_workers}")
+            ##workers = Worker.all(connection=redis_connection) # Returns the actual worker objects
+            ##logging.debug(f"Total rq workers ({len(workers)}): {workers}")
+            ##our_queue_workers = Worker.all(queue=our_queue)
+            ##logging.debug(f"Our {our_callback_name} queue workers ({len(our_queue_workers)}): {our_queue_workers}")
 
-            #worker_count = Worker.count(connection=redis_connection)
-            #logging.debug(f"Total rq workers = {worker_count}")
-            #our_queue_worker_count = Worker.count(queue=our_queue)
-            #logging.debug(f"Our {our_callback_name} queue workers = {our_queue_worker_count}")
+            ##worker_count = Worker.count(connection=redis_connection)
+            ##logging.debug(f"Total rq workers = {worker_count}")
+            ##our_queue_worker_count = Worker.count(queue=our_queue)
+            ##logging.debug(f"Our {our_callback_name} queue workers = {our_queue_worker_count}")
 
-            info_message = f'{OUR_NAME} queued valid callback job to {our_callback_name} ' \
-                        f'({len_our_queue} jobs now ' \
-                            f'for {Worker.count(queue=our_queue)} workers, ' \
-                        f'{len(other_callback_queue)} jobs in {other_our_adjusted_callback_name} queue ' \
-                            f'for {Worker.count(queue=other_callback_queue)} workers, ' \
-                        f'{len_failed_queue} failed jobs) at {datetime.utcnow()}'
-            logging.info(info_message)
-            return f'{OUR_NAME} queued valid callback job to {our_callback_name} at {datetime.utcnow()}'
-        else:
-            stats_client.incr('InvalidCallbackPostsReceived')
-            error_message = f'{OUR_NAME} ignored invalid callback payload; responding with {response_dict}'
-            logging.error(error_message)
-            return error_message, 400
-# end of callback_receiver()
+            #info_message = f'{OUR_NAME} queued valid callback job to {our_callback_name} ' \
+                        #f'({len_our_queue} jobs now ' \
+                            #f'for {Worker.count(queue=our_queue)} workers, ' \
+                        #f'{len(other_callback_queue)} jobs in {other_our_adjusted_callback_name} queue ' \
+                            #f'for {Worker.count(queue=other_callback_queue)} workers, ' \
+                        #f'{len_failed_queue} failed jobs) at {datetime.utcnow()}'
+            #logging.info(info_message)
+            #return f'{OUR_NAME} queued valid callback job to {our_callback_name} at {datetime.utcnow()}'
+        #else:
+            #stats_client.incr('InvalidCallbackPostsReceived')
+            #error_message = f'{OUR_NAME} ignored invalid callback payload; responding with {response_dict}'
+            #logging.error(error_message)
+            #return error_message, 400
+## end of callback_receiver()
 
 
 if __name__ == '__main__':
