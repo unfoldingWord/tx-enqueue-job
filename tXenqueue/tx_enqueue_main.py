005b8028005b751086d5e5bf1de7aff20af358e2
Robert Hunt <Freely.Given.org@gmail.com>
2018-10-09 13:47:39 +1300

    Increase logging level

--- tXenqueue/tx_enqueue_main.py
+++ tXenqueue/tx_enqueue_main.py
@@ -50,7 +50,7 @@ else:
 
 
 # Enable DEBUG logging for dev- instances (but less logging for production)
-logging.basicConfig(level=logging.DEBUG if prefix else logging.ERROR)
+logging.basicConfig(level=logging.DEBUG if prefix else logging.INFO)
 
 prefix_string = f" with prefix {prefix!r}" if prefix else ""
 logging.info(f"enqueueMain.py running on Python v{sys.version}{prefix_string}")
